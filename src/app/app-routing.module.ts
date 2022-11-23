486793433d0ad72a6cf333a9577eefb1395681d7
Dan <florentius4@gmail.com>
2022-11-23 13:50:44 -0700

    upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Cmp1Component } from './components/cmp1/cmp1.component';
+import { Cmp2Component } from './components/cmp2/cmp2.component';
+import { Cmp3Component } from './components/cmp3/cmp3.component';
+import { WildcardComponent } from './components/wildcard/wildcard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'cpm1', component: Cmp1Component},
+  {path: 'cpm2', component: Cmp2Component},
+  {path: 'cpm3', component: Cmp3Component},
+  {path: '**', component: WildcardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { Cmp1Component } from './components/cmp1/cmp1.component';
+import { Cmp2Component } from './components/cmp2/cmp2.component';
+import { Cmp3Component } from './components/cmp3/cmp3.component';
+import { WildcardComponent } from './components/wildcard/wildcard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    Cmp1Component,
+    Cmp2Component,
+    Cmp3Component,
+    WildcardComponent
   ],
   imports: [
     BrowserModule,
